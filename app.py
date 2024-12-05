ae1c54e779d1d47d3c25c54fb084e2a39c5f71ca
zamo23 <Lz7151919@gmail.com>
2024-12-05 15:25:23 -0500

    agregar lista de comando

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import os
-from telegram import Update
+from telegram import Update, BotCommand
 from dotenv import load_dotenv
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 load_dotenv()
 TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+commands = [
+    BotCommand("start", "Inicia el bot"),
+    BotCommand("chatid", "Obtén tu chat ID"),
+]
+
+async def set_commands(application: Application) -> None:
+    bot = application.bot
+    await bot.set_my_commands(commands)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('¡Hola! Envía /chatid para obtener tu chat ID.')
@@ -13,13 +22,21 @@ async def chatid(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.message.chat_id
     await update.message.reply_text(f'Tu chat ID es: `{chat_id}`', parse_mode='Markdown')
 
-def main() -> None:
+async def main() -> None:
     application = Application.builder().token(TOKEN).build()
+
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("chatid", chatid))
 
-    print("Bot está corriendo correctamente...") 
-    application.run_polling(stop_signals=None)
+    await set_commands(application)
+
+    print("Bot está corriendo correctamente...")
+    await application.run_polling(stop_signals=None)
 
 if __name__ == '__main__':
-    main()
+    print("Bot está corriendo correctamente...")
+
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("chatid", chatid))
+    application.run_polling(stop_signals=None)
